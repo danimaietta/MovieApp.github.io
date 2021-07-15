801526b0762605b7df5928ec1d148c6acc150c16
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2021-07-15 04:07:24 -0600

    fix mail data

--- app/utils/email.js
+++ app/utils/email.js
@@ -2,19 +2,19 @@ import { send, init } from 'emailjs-com'
 import { months } from './utils'
 
 init('user_xGmxuVGkTxsGlfO5Nke6f')
-const sendEmail = ({ movie, price, seats, date, hour, email }) => {
+const sendEmail = ({ movie, price, seatNames, date, hour, email }) => {
   return send(
     'service_h10tdd8',
     'template_wjrhujl',
     {
       movie,
       price,
-      seats,
+      seatNames: seatNames.toString(),
       day: date[1],
       hour,
       month: months[date[0]],
       email,
-      code: `${seats} - ${Math.random() * 10}`
+      code: `${seatNames.toString()} - ${Math.random() * 10}`
     },
     'user_xGmxuVGkTxsGlfO5Nke6f'
   )
