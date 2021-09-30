d5e9d153ec8f13d499078b0c491c9626d585e860
Daniel Maietta Esquivel <danimaietta1@gmail.com>
2021-09-29 23:38:50 -0600

    final test flashLight

--- __tests__/flashLight.spec.js
+++ __tests__/flashLight.spec.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { screen } from '@testing-library/dom'
-import { render, cleanup, prettyDOM } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import FlashLight from '../app/components/Movies/FlashLight'
 import LocaleContext from '../app/context/LocaleContext'
+
+const setTheme = jest.fn()
+
+function flashLight(theme) {
+  render(
+    <LocaleContext.Provider value={{ theme, setTheme }}>
+      <FlashLight />
+    </LocaleContext.Provider>
+  )
+}
 
 afterEach(cleanup)
 
 describe('Flash Light', () => {
   test('Check if theme is dark then display 💡', () => {
-    const theme = 'dark'
-    render(
-      <LocaleContext.Provider value={{ theme }}>
-        <FlashLight />
-      </LocaleContext.Provider>
-    )
+    flashLight('dark')
     expect(screen.getByText(/💡/i)).toBeTruthy()
   })
   test('Check if theme is light then display 🔦', () => {
-    const theme = 'light'
-    render(
-      <LocaleContext.Provider value={{ theme }}>
-        <FlashLight />
-      </LocaleContext.Provider>
-    )
+    flashLight('light')
     expect(screen.getByText(/🔦/i)).toBeTruthy()
+  })
+  test('Check if clicking the button it toggles to dark again', () => {
+    flashLight('light')
+    fireEvent.click(screen.getByText(/🔦/i))
+    expect(setTheme).toHaveBeenCalledTimes(1)
   })
 })
